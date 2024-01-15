7ccf14242206f67a887d9a148a7286cd0b79e8c1
Yahia Abukhalil <yahiamrvc@gmail.com>
2024-01-15 23:39:56 +0200

    edited main to be to load modules

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
-import modules.load_utils
-import modules.plot_utils
-import modules.train_utils
+import sys
+import os
 
+# Add the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import modules.load_utils as loader
+import modules.plot_utils as plotter
+import modules.train_utils as trainer
+
+# Rest of your code
+
+data = loader.load_csv("titanic/train.csv")
+
+print(data.head())
